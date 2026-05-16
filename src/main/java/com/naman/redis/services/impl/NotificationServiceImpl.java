d2b5dcd3622c173f363ff7459ac46c1ea20a085c
Naman Sureka <namansureka5@gmail.com>
2026-05-16 21:26:45 +0530

    feat: implement notification queuing and batching core logic

--- src/main/java/com/naman/redis/services/impl/NotificationServiceImpl.java
+++ src/main/java/com/naman/redis/services/impl/NotificationServiceImpl.java
@@ -5,13 +5,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
     private static final int COOLDOWN_MINUTES = 15;
 
     @Override
@@ -27,17 +29,55 @@ public class NotificationServiceImpl implements NotificationService {
                 TimeUnit.MINUTES
         );
 
-        if(Boolean.TRUE.equals(canSendImmediately)){
+        if (Boolean.TRUE.equals(canSendImmediately)) {
 
             System.out.println("Push Notification Sent to User : " + message);
-        }
-        else{
+        } else {
             redisTemplate.opsForList().rightPush(pendingKey, message);
+            redisTemplate.opsForSet().add("pending_users", userId);
         }
     }
 
     @Override
     public void processPendingNotifications() {
 
+        Set<Object> users = redisTemplate.opsForSet().members("pending_users");
+        if (users == null|| users.isEmpty()) return;
+
+        for (Object userObj : users) {
+
+            Long userId = Long.valueOf(userObj.toString());
+            String pendingKey = "user:" + userId + ":pending_notifs";
+            String processingKey = "user:" + userId + ":processing_notifs";
+
+            if(Boolean.FALSE.equals(redisTemplate.hasKey(pendingKey))){
+                continue;
+            }
+
+            redisTemplate.rename(pendingKey, processingKey);
+
+            List<Object> notifications = redisTemplate.opsForList().range(processingKey, 0, -1);
+
+            if(notifications == null || notifications.isEmpty()){
+
+                redisTemplate.delete(processingKey);
+                continue;
+            }
+            String firstNotification = notifications.get(0).toString();
+
+            int othersCount = notifications.size() - 1;
+            if(othersCount == 0){
+                System.out.println("Summarized Push Notification : " + firstNotification);
+
+            }
+            else{
+
+                System.out.println("Summarized Push Notification : " + firstNotification + " and "
+                        + othersCount + " others interacted with your posts"
+                );
+            }
+            redisTemplate.delete(processingKey);
+            redisTemplate.opsForSet().remove("pending_users", userId);
+        }
     }
 }
